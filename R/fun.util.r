ba938ca0221aac590ef5bdf327a2c9a6cd501ce6
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2013-05-27 19:34:02 +0100

    fixing error in storing of infos

--- R/fun.util.r
+++ R/fun.util.r
@@ -70,7 +70,7 @@ evaluateParameters <- function(ps,cf,balance=FALSE) {
       # collect the addititonal infos
       if (length(val$infos)>0) {
         rd.infos = data.frame(val$infos)
-        colnames(rd.infos) <- paste(colnames('d',rd.infos,sep='.'))
+        colnames(rd.infos) <- paste('d',colnames(rd.infos),sep='.'))
         rd = cbind(rd,rd.infos)
       }
 
