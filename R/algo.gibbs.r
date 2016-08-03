b0d57c69afd395f39ddef5566556471447d8cf3a
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2016-08-03 11:51:54 -0500

    more information displayed

--- R/algo.gibbs.r
+++ R/algo.gibbs.r
@@ -18,7 +18,7 @@ algo.gibbs <- function(rd,param_data,niter,cf,pdesc,priv) {
     cf$current_param = "none"
     priv = cf
     priv$N=1
-    return(list(nps = computeInitialCandidates(N,cf) , ndt = rd,cf=cf ))
+    return(list(ps = computeInitialCandidates(N,cf) , evals=rd,priv=priv ))
   }
 
   # we draw the new parameter value according to the posterior probabilities
@@ -27,12 +27,26 @@ algo.gibbs <- function(rd,param_data,niter,cf,pdesc,priv) {
   best = rd[rd$chain==new_param_i,]
   best$chain=0
   rr = rbind(rr,best)
+
+  # we display the evaluations
+  if ("current_param"%in%names(priv)) {
+    sel = rep(" ",N)
+    sel[new_param_i]="*"
+    flog.info("evaluations for %s",priv$current_param)
+    v = sprintf("%+3.3f%s",as.numeric(rd[paste("p.",priv$current_param,sep="")][,1]),sel)
+    flog.info("x= %s", paste(v,collapse="  "))
+    v = sprintf("%+3.3f%s",as.numeric(rd["value"][,1]),sel)
+    flog.info("f= %s",paste(v,collapse="  "))
+    v = sprintf("%+3.3f%s",exp(-rd$value)/sum(exp(-rd$value)),sel)
+    flog.info("p= %s",paste(v,collapse="  "))
+  }
   
   # we randomly select a parameter
   param = sample(cf$params_to_sample, 1)
-  grid = cf$pdesc[param,'lb'] + (cf$pdesc[param,'ub']-cf$pdesc[param,'lb'])* ((seq(1/N,1,l=N) + rnorm(1)) %% 1)
+  grid = sort(cf$pdesc[param,'lb'] + (cf$pdesc[param,'ub']-cf$pdesc[param,'lb'])* ((seq(1/N,1,l=N) + rnorm(1)) %% 1))
   priv$current_param = param
-  
+  flog.info("nest param to evaluate: %s",param)
+
   # the next evaluations are just a uniform sequence from the bounds
   val_new = rd[rd$chain==new_param_i,]
   for (c.current in 1:N) {
@@ -46,6 +60,9 @@ algo.gibbs <- function(rd,param_data,niter,cf,pdesc,priv) {
     p.new[[param]]          = grid[c.current]
     ps[[c.current]]         = p.new
   }
- 
+  
+  v = sprintf("%+3.3f",grid)
+  flog.info("support= %s",paste(v,collapse="  "))
+
    return(list(ps = ps , evals = rr, priv=priv ))
 }
